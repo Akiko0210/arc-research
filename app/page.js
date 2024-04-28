779c444378f9481789e0c6cad832790294285f4a
Akiko0210 <qea643@gmail.com>
2024-04-27 19:59:47 -0700

    border animation

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
+// import "./page.scss";
 
 export default function Home() {
   return (
     <div className="h-screen flex justify-center items-center relative">
-      <div className="text-9xl">Arc research</div>
+      <div className="text-9xl border-gradient-to-r from-[#d53e33] to-[#fbb300] ">
+        Arc research
+      </div>
+      {/* add box class if you need noen moving border */}
       <input
-        className="rounded p-4 w-[700px] absolute bg-background backdrop-blur-lg"
+        className="box rounded p-4 w-[700px] bg-background backdrop-blur-lg absolute"
         placeholder="Search..."
       />
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        // "conic-gradient(from 180deg,#d53e33 0deg 90deg,#fbb300 90deg 180deg,#377af5 180deg 270deg,#399953 270deg 360deg)",
       },
       colors: {
         background: "rgba(7, 10, 36, 0.1)",
